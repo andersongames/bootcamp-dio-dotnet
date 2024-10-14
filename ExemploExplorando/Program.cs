e8e3d0f0b8a6c4867159ca42fab7ed56aab471d2
andersongames <andersonpgames@gmail.com>
2024-10-14 11:58:27 -0300

    feat: certificates

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa("Anderson", "Games");
+// p1.Nome = "Anderson";
+// p1.Sobrenome = "Games";
+
+Pessoa p2 = new Pessoa(nome: "Eduardo", sobrenome: "Queiroz");
+// p2.Nome = "Eduardo";
+// p2.Sobrenome = "Queiroz";
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
